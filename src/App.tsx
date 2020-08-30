fd166ed942b71dcb29a3a1de45a49b4f1922faf6
Flynn <wphestiraid@gmail.com>
2020-08-30 19:11:55 +0900

    기본 Route 구조 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './routes/Home';
 import GlobalStyle from './GlobalStyle';
 
 const App: React.FunctionComponent = () => {
   return (
     <>
       <GlobalStyle />
-      <div className="App">Movie.log</div>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Router>
     </>
   );
 };
