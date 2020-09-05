5e5da7b456a6d0c6c7811997769314e129f5f5fe
Flynn <wphestiraid@gmail.com>
2020-09-05 23:56:57 +0900

    Header 스타일링 중

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,19 +6,38 @@ const HeaderContainer = styled.header`
   height: 64px;
   padding: 0 50px;
   line-height: 64px;
+  background: ${(props) => props.theme.background.paper};
+  box-shadow: ${(props) => props.theme.shadow};
+  display: flex;
+  flex-direction: row;
 `;
 
-const LogoContainer = styled.div``;
+const LogoContainer = styled.div`
+  align-items: center;
+  margin-right: 32px;
+`;
+
+const Logo = styled.h1`
+  font-size: 32px;
+`;
 
 const MenuContainer = styled.div``;
+
+const Menu = styled(Link)`
+  text-decoration: none;
+  color: ${(props) => props.theme.typography.primary};
+  padding: 0 20px;
+`;
 
 const Header: React.FC = () => {
   return (
     <HeaderContainer>
-      <LogoContainer>Movie.log</LogoContainer>
+      <LogoContainer>
+        <Logo>Movie.log</Logo>
+      </LogoContainer>
       <MenuContainer>
-        <Link to="/search">Search</Link>
-        <Link to="/profile">Profile</Link>
+        <Menu to="/search">Search</Menu>
+        <Menu to="/profile">Profile</Menu>
       </MenuContainer>
     </HeaderContainer>
   );
--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -1,7 +1,9 @@
 export interface CustomThemeInterface {
   background: {
     default: string;
+    paper: string;
   };
+  shadow: string;
   typography: {
     primary: string;
     secondary: string;
@@ -12,7 +14,10 @@ export interface CustomThemeInterface {
 const lightTheme: CustomThemeInterface = {
   background: {
     default: '#fafafa',
+    paper: '#ffffff',
   },
+  shadow:
+    '0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)',
   typography: {
     primary: 'rgba(0, 0, 0, 0.87)',
     secondary: 'rgba(0, 0, 0, 0.54)',
@@ -23,7 +28,10 @@ const lightTheme: CustomThemeInterface = {
 const darkTheme: CustomThemeInterface = {
   background: {
     default: '#303030',
+    paper: '#424242',
   },
+  shadow:
+    '0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)',
   typography: {
     primary: '#fff',
     secondary: 'rgba(255, 255, 255, 0.7)',
