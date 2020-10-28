fce0603c83948bb84d50726158a2c1ca1bbbb2d7
Flynn <wphestiraid@gmail.com>
2020-10-28 10:52:39 +0900

    검색 페이지 생성 및 스타일링

---
+++ src/pages/search.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import Head from 'components/Head';
+
+const Search: React.FC = () => {
+  const { query } = useRouter();
+  const result: Movie[] = [];
+
+  const title = query?.q ? `'${query?.q}' 검색 결과` : undefined;
+
+  if (!result.length) {
+    return (
+      <EmptyContainer>
+        <EmptyResult>
+          검색 결과가 없습니다. 다른 검색어로 검색해보세요.
+        </EmptyResult>
+      </EmptyContainer>
+    );
+  }
+
+  return (
+    <>
+      <Head title={title} />
+      Search
+    </>
+  );
+};
+
+const EmptyContainer = styled.div`
+  display: flex;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  justify-content: center;
+  align-items: center;
+`;
+
+const EmptyResult = styled.div`
+  color: ${({ theme }) => theme.typography.secondary};
+`;
+
+export default Search;
