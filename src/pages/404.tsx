4a0f217458a884ed12f9e7f00e4e02b1dfada1e9
Flynn <wphestiraid@gmail.com>
2020-11-17 10:33:22 +0900

    404 페이지 간단 스타일링

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import Head from 'components/Head';
+import styled from 'styled-components';
 
 const NotFound: React.FC = () => {
   return (
     <>
       <Head title={'페이지를 찾을 수 없습니다'} />
-      페이지를 찾을 수 없습니다.
+      <NotFoundContainer>
+        <NotFoundText>페이지를 찾을 수 없습니다.</NotFoundText>
+      </NotFoundContainer>
     </>
   );
 };
+
+const NotFoundContainer = styled.div`
+  display: flex;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NotFoundText = styled.div`
+  color: ${({ theme }) => theme.typography.secondary};
+`;
 
 export default NotFound;
